11dbd91d60a953376179defcb7c2ced9d67c44b2
ed <null@bt-room>
2016-01-11 09:33:50 -0800

    initial commit

---
+++ logic_gates.rb
@@ -0,0 +1,170 @@
+module And
+	class Double
+		attr_accessor :input_a, :input_b
+    def initialize
+      @input_a = 0
+      @input_b = 0
+    end
+		def output
+			puts (@input_a + @input_b) == 2
+		end
+	end
+	class Triple
+		attr_accessor :input_a, :input_b, :input_c
+    def initialize
+      @input_a = 0
+      @input_b = 0
+      @input_c = 0
+    end
+		def output
+			puts (@input_a + @input_b + @input_c) == 3
+		end
+	end
+	class Multi
+    def initialize(num)
+      @variables = []
+      @total = 0
+      @sum = 0
+      @num = num
+    end
+    def process(vars)
+      @sum = 0
+      @total = vars.length
+      #puts "total = #{total}"
+      vars.each.with_index{|val|
+        #puts "val = #{val} = #{instance_variable_get(val)}" 
+        @sum += instance_variable_get(val)
+      }
+      #puts "sum = #{@sum}"
+    end
+    def output
+      if @num == @total
+        puts @total == @sum 
+      else
+        puts 'false'
+      end
+    end
+    def method_missing(name,*args, &block)
+
+      #puts " #{name} was called "
+      #puts "name = #{name}"
+      variable = '@' + name.to_s.split('_')[1].gsub(/\=/,'')
+      instance_variable_set(variable,args[0])
+      @variables << variable
+      process(@variables) if name != "total"
+    end
+	end
+end
+
+module Or
+  class Double
+    attr_accessor :input_a, :input_b
+    def initialize
+      @input_a = 0
+      @input_b = 0
+    end
+    def output
+      puts (@input_a + @input_b) > 0
+    end
+  end
+  class Triple
+    attr_accessor :input_a, :input_b, :input_c
+    def initialize
+      @input_a = 0
+      @input_b = 0
+      @input_c = 0
+    end
+    def output
+      puts (@input_a + @input_b + @input_c) > 0
+    end
+  end
+  class Multi
+    def initialize(num)
+      @variables = []
+      @total = 0
+      @sum = 0
+      @num = num
+    end
+    def process(vars)
+      @sum = 0
+      @total = vars.length
+      #puts "total = #{total}"
+      vars.each.with_index{|val|
+        #puts "val = #{val} = #{instance_variable_get(val)}" 
+        @sum += instance_variable_get(val)
+      }
+      #puts "sum = #{@sum}"
+    end
+    def output
+      if @num == @total
+        puts @total > 0 
+      else
+        puts 'false'
+      end
+    end
+    def method_missing(name,*args, &block)
+
+      #puts " #{name} was called "
+      #puts "name = #{name}"
+      variable = '@' + name.to_s.split('_')[1].gsub(/\=/,'')
+      instance_variable_set(variable,args[0])
+      @variables << variable
+      process(@variables) if name != "total"
+    end
+  end
+end
+
+#---------- AND
+gate = And::Double.new
+gate.input_a = 1
+gate.input_b = 1
+gate.output # => 1
+gate.input_b = 0
+gate.output # => 0
+
+gate = And::Triple.new
+gate.input_a = 1
+gate.input_b = 1
+gate.output # => 0
+gate.input_c = 1
+gate.output # => 1
+gate.input_a = 0
+gate.output # => 0
+
+gate = And::Multi.new(4)
+gate.input_a = 1
+gate.input_b = 1
+gate.input_c = 1
+gate.output # => 0
+gate.input_d = 1
+gate.output # => 1
+gate.input_b = 0
+gate.output # => 0
+
+#---------- Or
+
+gate = Or::Double.new
+gate.input_a = 1
+gate.input_b = 0
+gate.output # => 1
+gate.input_a = 0
+gate.output # => 0
+
+gate = Or::Triple.new
+gate.input_a = 0
+gate.input_b = 0
+gate.output # => 0
+gate.input_c = 1
+gate.output # => 1
+gate.input_c = 0
+gate.output # => 0
+
+gate = Or::Multi.new(4)
+gate.input_a = 0
+gate.input_b = 0
+gate.input_c = 0
+gate.output # => 0
+gate.input_d = 1
+gate.output # => 1
+gate.input_d = 0
+gate.output # => 0
